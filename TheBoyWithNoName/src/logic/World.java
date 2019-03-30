220b2a9d9742ac2840d6889ea8d2599357b62983
Iman Abubakar <s1891419@vervcelli.inf.ed.ac.uk>
2019-03-30 20:31:42 +0000

    boy modified

--- TheBoyWithNoName/src/logic/World.java
+++ TheBoyWithNoName/src/logic/World.java
@@ -61,6 +61,7 @@ public class World {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        
 
         // The second line contains colour configuration
         // We ignore this and any other lines until the map data completely
